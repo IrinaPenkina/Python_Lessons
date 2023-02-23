545b8616af469a8311a0d95437fadf4ded13b524
IrinaPenkina <nikolay_irina@mail.ru>
2023-02-23 21:42:34 +0300

    lesson 2 done

--- Python_Course/main_2_lists.py
+++ Python_Course/main_2_lists.py
@@ -49,11 +49,30 @@
 # print(list_1)  # [4, 7, 12, 1, 9]
 
 # Срезы списка
-list_1 = [4, 7, 1, 9, 2, 13]
-print(list_1[:])  # весь
-print(list_1[:2])  # два первых элемента
-print(list_1[len(list_1)-2:]) # два последних элемента
-print(list_1[::2]) # с нулевого через один до конца
+# list_1 = [4, 7, 1, 9, 2, 13]
+# print(list_1[:])  # весь
+# print(list_1[:2])  # два первых элемента
+# print(list_1[len(list_1)-2:]) # два последних элемента
+# print(list_1[::2]) # с нулевого через один до конца
+
+
+# Быстрое задание списка: list comprehension
+list_2 = [3 for i in range(5)]
+print(list_2)
+
+# Задать список из последовательности чисел от 1 до 10
+list_3 = [i for i in range (1, 11)]
+print(list_3)
+
+# Задать список из последовательности четных чисел от 1 до 10
+list_4 = [i for i in range (1, 11) if i % 2 == 0]
+print(list_4)
+
+# Задать пары четных чисел от 1 до 20 через кортеж
+list_5 = [(i, i) for i in range (1, 21) if i % 2 == 0]
+print(list_5)
+list_6 = [(i, i*i) for i in range (1, 21) if i % 2 == 0]
+print(list_6)
 
 
 
